20d5b7bc93c7186065780fbf548ad531918e9666
gabbosx77 <gabriel.navarro@alumnos.udg.mx>
2019-03-19 23:35:01 -0600

    Menu lateral funcional y paginas creadas

---
+++ src/app/register-car/register-car.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-car',
+  templateUrl: './register-car.page.html',
+  styleUrls: ['./register-car.page.scss'],
+})
+export class RegisterCarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
