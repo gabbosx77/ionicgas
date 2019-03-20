ef5b68af83c7d61d943cb2f4fa6ee84230c78144
vinz <lazvinz21@gmail.com>
2019-03-19 22:18:10 -0600

    dsasdd

--- backend/Back/hello.py
+++ backend/Back/hello.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
-from py2neo.ogm import GraphObject, Property, RelatedTo
+from py2neo import Graph
 app = Flask(__name__)
  
+
 @app.route("/")
 def hello():
     return render_template('home.page.html')
  
 if __name__ == "__main__":
+    graph = Graph("bolt://localhost:7687",auth=("neo4j","1234"))
+    
+    resultado = graph.run("MATCH (a) RETURN a;")
+    #n=0
+    #for rec in resultado:
+    #    print(rec["a"])
+    #    n = n+1
+    #print("registros totales"+ str(n))
     app.run()
+
 
