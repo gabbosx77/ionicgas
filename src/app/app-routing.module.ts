3c2703448fdeb9a196d34d1009aae4417bb6f358
gabbosx77 <gabriel.navarro@alumnos.udg.mx>
2019-05-03 23:19:59 -0500

    security on login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { NoaccesoGuard } from './guards/noacceso.guard';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule' },
+  { path: '', redirectTo: 'acceso', pathMatch: 'full' },
+  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule', canActivate : [AuthGuard]  },
   { path: 'first-config', loadChildren: './first-config/first-config.module#FirstConfigPageModule' },
   { path: 'vehiculos', loadChildren: './vehiculos/vehiculos.module#VehiculosPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule', canActivate : [AuthGuard] },
   { path: 'singin', loadChildren: './singin/singin.module#SinginPageModule' },
-  { path: 'estadistica', loadChildren: './estadistica/estadistica.module#EstadisticaPageModule' },  { path: 'acceso', loadChildren: './acceso/acceso.module#AccesoPageModule' },
+  { path: 'estadistica', loadChildren: './estadistica/estadistica.module#EstadisticaPageModule' },
+  { path: 'acceso', loadChildren: './acceso/acceso.module#AccesoPageModule', canActivate : [NoaccesoGuard]  },
 
 
 
